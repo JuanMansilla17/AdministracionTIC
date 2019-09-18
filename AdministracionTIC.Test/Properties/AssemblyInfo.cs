494e92765e8f3ac781b53ddec700bb5512b6d393
JuanPMansilla <juampimansilla17@gmail.com>
2019-09-18 19:20:50 -0300

    Los test funcionan

---
+++ AdministracionTIC.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdministracionTIC.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdministracionTIC.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aff9188e-4657-4aac-ac3b-55eba84eb3ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AdministracionTIC.Test/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdministracionTIC.Test
+{
+    [TestClass]
+    public class FechaHoraPrestamo
+    {
+        //GestorPrestamo gestorPrestamo = new GestorPrestamo();
+
+        [TestMethod]
+        public void FechaHora_SeConstruyenCorrectamente()
+        {
+            //Arrange
+            GestorPrestamo gestorPrestamo = new GestorPrestamo();
+
+            DateTime fecha = new DateTime(2019, 4, 17);
+            String hora = "12";
+            String minuto = "30";
+            DateTime fechaHoraEsperada = new DateTime(2019, 4, 17, 12, 30, 0);
+
+            //Act
+            DateTime fechaHora = gestorPrestamo.ConstruirFechaHora(fecha, hora, minuto);
+
+            //Assert
+            Assert.AreEqual(fechaHoraEsperada, fechaHora);
+        }
+
+        [TestMethod]
+        public void FechaPrestamo_NoSuperaLaFechaLimite()
+        {
+
+            //Arrange
+            //VistaPrestamo vistaPrestamo = new VistaPrestamo();
+            GestorPrestamo gestorPrestamo = new GestorPrestamo();
+
+            DateTime fecha = DateTime.Now.AddDays(32);
+
+            //Act
+            Boolean fechaValida = gestorPrestamo.FechaValida(fecha);
+
+            //Assert
+            Assert.IsTrue(fechaValida);
+
+        }
+
+        [TestMethod]
+        public void DetectaHorario_FueraDeRangoDeHorayMinuto()
+        {
+            String horaEntrega = "22";
+            String minutoEntrega = "0";
+            String horaDevolucion = "22";
+            String minutoDevolucion = "30";
+
+            //VistaPrestamo vistaPrestamo = new VistaPrestamo();
+            GestorPrestamo gestorPrestamo = new GestorPrestamo();
+
+            Boolean horarioValido = gestorPrestamo.HorarioValido(horaEntrega,
+                minutoEntrega,
+                horaDevolucion,
+                minutoDevolucion);
+
+            Assert.IsTrue(horarioValido);
+        }
+    }
+}
